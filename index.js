b6c34560c65d8c4ab99132a061bce1f9020c10e8
wnp <->
2024-09-10 07:19:11 +0700

    -

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(
   "/api-docs",
   swaggerUi.serve,
-  swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL })
+  swaggerUi.setup(swaggerDocs, {
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCssUrl: CSS_URL,
+  })
 );
 
 const logger = (req, res, next) => {
